1274024b426f5786e9ca14f1aa18ec366807bab6
OkiMaureen <oki.maureen.eloho@gmail.com>
2018-04-24 10:45:38 +0100

    add get all meals api endpoint [#157036410]

--- server/app.js
+++ server/app.js
@@ -4,8 +4,11 @@ import express from 'express';
 // importing bodyparser
 import bodyParser from 'body-parser';
 
+// importring routes module
+import routes from './routes';
+
 // Defining the Port Variable
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8020;
 
 // Instantiating express
 const app = new express();
@@ -14,12 +17,12 @@ const app = new express();
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+routes(app);
 
 // Starting up the server
 app.listen(port);
 
 // Console message
-console.log(`server running at http://localhost:${port}`);
+console.log(`server is running at http://localhost:${port}`);
 
 export default app;
